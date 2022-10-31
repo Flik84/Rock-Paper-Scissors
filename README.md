38725ff9907e0883c8278719588340ceae5ea40d
Flik84 <chris.polvi@hotmail.ca>
2022-10-31 16:12:29 -0700

    Create function that plays five rounds of game and declares winner or draw

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock-Paper-Scissors
+
+An exciting version of the classic game that runs in your browser! Face off against the computer and see if you come out on top after five matches. 
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-
-
+// generates random number between 1 and 3 for computer choice (for use in game() function below)
 const getComputerChoice = function () {
     const choice = Math.floor(Math.random() * 3 + 1);
     if (choice === 1) {
@@ -10,14 +9,43 @@ const getComputerChoice = function () {
     } else return 'scissors';
 }
 
+// plays a single round of game against computer (for use in game() function below)
 const playRound = function (playerSelect, computerSelect) {
-    playerSelectLower= playerSelect.toLowerCase();
-if (playerSelectLower === 'rock' && computerSelect === 'scissors' || playerSelectLower === 'paper' && computerSelect === 'rock' || playerSelectLower === 'scissors' && computerSelect === 'paper') {
-    return `You win! ${playerSelectLower} beats ${computerSelect}`;
-} else if (playerSelectLower === 'rock' && computerSelect === 'paper' || playerSelectLower === 'paper' && computerSelect === 'scissors' || playerSelectLower === 'scissors' && computerSelect === 'rock') {
-    return `You lose! ${computerSelect} beats ${playerSelectLower}`;
-} else return `Draw! You both chose ${playerSelectLower}.`;
+    const player = playerSelect.toLowerCase();
+
+if (player === 'rock' && computerSelect === 'scissors' || player === 'paper' && computerSelect === 'rock' || player === 'scissors' && computerSelect === 'paper') {
+    console.log(`You win! ${player} beats ${computerSelect}`); 
+    return 'win';
+} else if (player === 'rock' && computerSelect === 'paper' || player === 'paper' && computerSelect === 'scissors' || player === 'scissors' && computerSelect === 'rock') {
+    console.log(`You lose! ${computerSelect} beats ${player}`); 
+    return 'lose';
+} else {
+    console.log(`Draw! You both chose ${player}.`);
+    return 'draw';
+}
 }
 
-const playerSelect = prompt('Choose rock, paper or scissors: ')
+// plays five rounds of game against computer and displays results at end
+const game = function () {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const choicePrompt = prompt('Choose rock, paper or scissors: ');
+        const round = playRound(choicePrompt, getComputerChoice());
+        if (round === 'win') {
+            playerScore++;
+        } else if (round === 'lose') {
+            computerScore++;
+        }
+    }; 
 
+    if (playerScore > computerScore) {
+        console.log('');
+        console.log(`You WIN the game ${playerScore} to ${computerScore}!`);
+    } else if (computerScore > playerScore) {
+        console.log('');
+        console.log(`You LOSE the game ${playerScore} to ${computerScore}!`);
+    } else console.log(`This game was a DRAW, you both scored ${playerScore} points!`);
+}
+
+game();
