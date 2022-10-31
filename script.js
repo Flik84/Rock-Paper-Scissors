149a063e3f53a8dbeb078766af34abd724c57083
Flik84 <chris.polvi@hotmail.ca>
2022-10-30 17:55:57 -0700

    Create function that plays round of game using input from user and random input from computer

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 
 const getComputerChoice = function () {
     const choice = Math.floor(Math.random() * 3 + 1);
-    console.log(choice);
     if (choice === 1) {
         return 'rock';
     } else if (choice === 2) {
@@ -11,4 +10,14 @@ const getComputerChoice = function () {
     } else return 'scissors';
 }
 
-console.log(getComputerChoice());
+const playRound = function (playerSelect, computerSelect) {
+    playerSelectLower= playerSelect.toLowerCase();
+if (playerSelectLower === 'rock' && computerSelect === 'scissors' || playerSelectLower === 'paper' && computerSelect === 'rock' || playerSelectLower === 'scissors' && computerSelect === 'paper') {
+    return `You win! ${playerSelectLower} beats ${computerSelect}`;
+} else if (playerSelectLower === 'rock' && computerSelect === 'paper' || playerSelectLower === 'paper' && computerSelect === 'scissors' || playerSelectLower === 'scissors' && computerSelect === 'rock') {
+    return `You lose! ${computerSelect} beats ${playerSelectLower}`;
+} else return `Draw! You both chose ${playerSelectLower}.`;
+}
+
+const playerSelect = prompt('Choose rock, paper or scissors: ')
+
