f56ac900afb1dc981359550172475ee99b307b83
Flik84 <chris.polvi@hotmail.ca>
2022-12-03 13:23:51 -0800

    Add reset button so player can play again without refreshing browser

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
+'use strict';
+
 const buttons = document.querySelectorAll('.button');
 const resultsDisplay = document.querySelector('.results');
 const playerScoreDisplay = document.querySelector('#player-score');
 const computerScoreDisplay = document.querySelector('#computer-score');
-const startBtn = document.querySelector('.start-btn');
+const resetBtn = document.querySelector('.reset');
 
-const rockBtn = document.querySelector('#rock');
-const paperBtn = document.querySelector('#paper');
-const scissorsBtn = document.querySelector('#scissors');
+const rockBtn = document.querySelector('#Rock');
+const paperBtn = document.querySelector('#Paper');
+const scissorsBtn = document.querySelector('#Scissors');
+
+let playerScore = 0;
+let computerScore = 0;
+
+resetBtn.addEventListener('click', function () {
+    resultsDisplay.textContent = 'Make a selection to begin'
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreDisplay.textContent = `Player: ${playerScore}`;
+    computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+    enableButtons();
+})
 
 
 // generates random number between 1 and 3 for computer choice (for use in game() function below)
 const getComputerChoice = function () {
     const choice = Math.floor(Math.random() * 3 + 1);
     if (choice === 1) {
-        return 'rock';
+        return 'Rock';
     } else if (choice === 2) {
-        return 'paper';
-    } else return 'scissors';
+        return 'Paper';
+    } else return 'Scissors';
 }
 
 // plays a single round of game against computer (for use in game() function below)
 const playRound = function (playerSelect, computerSelect) {
-    const player = playerSelect.toLowerCase();
+    const player = playerSelect;
 
-if (player === 'rock' && computerSelect === 'scissors' || player === 'paper' && computerSelect === 'rock' || player === 'scissors' && computerSelect === 'paper') {
-    resultsDisplay.textContent = `You win! ${player} beats ${computerSelect}`; 
+if (player === 'Rock' && computerSelect === 'Scissors' || player === 'Paper' && computerSelect === 'Rock' || player === 'Scissors' && computerSelect === 'Paper') {
+    resultsDisplay.textContent = `You win! ${player} beats ${computerSelect}.`; 
     return 'win';
-} else if (player === 'rock' && computerSelect === 'paper' || player === 'paper' && computerSelect === 'scissors' || player === 'scissors' && computerSelect === 'rock') {
-    resultsDisplay.textContent = `You lose! ${computerSelect} beats ${player}`; 
+} else if (player === 'Rock' && computerSelect === 'Paper' || player === 'Paper' && computerSelect === 'Scissors' || player === 'Scissors' && computerSelect === 'Rock') {
+    resultsDisplay.textContent = `You lose! ${player} loses to ${computerSelect}.`; 
     return 'lose';
 } else {
     resultsDisplay.textContent =`Draw! You both chose ${player}.`;
@@ -42,27 +56,34 @@ const disableButtons = function () {
     scissorsBtn.disabled = true;
 }
 
+const enableButtons = function () {
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+}
+
 // allows player to play until player or computer reaches five wins and winner declared
 const play = function () {
-    let playerScore = 0;
-    let computerScore = 0;
-    resultsDisplay.textContent = 'Make a selection to begin'
     for (const button of buttons) {
-        button.addEventListener('click', function gameLogic () {
-            const result = playRound(button.id, getComputerChoice());
+        button.addEventListener('click', function () {
+            const computerChoice = getComputerChoice();
+            const playerChoice = button.id;
+            const result = playRound(playerChoice, computerChoice);
                 if (result === 'win') {
                     playerScore++;
                     playerScoreDisplay.textContent = `Player: ${playerScore}`;
                     if (playerScore === 5) {
-                        resultsDisplay.textContent = `You WIN the game!!!`;
-                        disableButtons();   
-                    }  
+                        console.log(playerScore);
+                        disableButtons();
+                        resultsDisplay.textContent = `${playerChoice} beats ${computerChoice}, you WIN the game!!!`;        
+                        }  
                 } else if (result === 'lose') {
                     computerScore++;
                     computerScoreDisplay.textContent = `Computer: ${computerScore}`;
                     if (computerScore === 5) {
-                        resultsDisplay.textContent = `You LOSE the game!!!`;
+                        console.log(computerScore);
                         disableButtons();
+                        resultsDisplay.textContent = `${playerChoice} loses to ${computerChoice}, you LOSE the game!!!`; 
                     }
                     
                     
